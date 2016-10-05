f56a572d24bb53be0e0445e3d5a6249f880e0238
joyceHong <joyceHong0827@gmail.com>
2016-10-05 14:39:10 +0800

    Day1HomeWork

---
+++ ProductCore/ProductCore.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace productTest
+{
+    public interface IProductCore
+    {
+        List<int> Sum<T>(List<T> numbers, string columnName, int maxCount);
+    }
+
+    public class ProductCore:IProductCore
+    {
+
+        public List<int> Sum<T>(List<T> numbers, string columnName, int maxCount)
+        {
+            List<int> result = new List<int>();
+            var number = 0;
+            var sum = 0;
+            var counter = 0;
+            numbers.ForEach(n =>
+            {
+                number =(int) n.GetType().GetProperty(columnName).GetValue(n, null);
+
+                sum += number;
+                ++counter;
+
+                if (counter == maxCount)
+                {
+                    result.Add(sum);
+                    counter = 0;
+                    sum = 0;
+                }
+            });
+
+            if (numbers.Count % maxCount != 0) {
+                result.Add(sum);
+            }
+            
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        public int id { get; set; }
+        public int cost { get; set; }
+        public int revenue { get; set; }
+        public int sellPrice { get; set; }
+     
+    }
+}
--- productTest/UnitTest1.cs
+++ productTest/UnitTest1.cs
@@ -1,14 +1,143 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using productTest;
+using System.Collections.Generic;
 
 namespace productTest
 {
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void 按照三組總合Cost的值()
         {
+            #region 假資料
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product()
+            {
+                id = 1,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 2,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 3,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 4,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 5,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+            #endregion
+
+            #region 期望值
+            List<int> expected = new List<int>() {
+                300,
+                200
+            };
+            #endregion
+
+            #region 結果
+            IProductCore productCoreObj = new ProductCore();
+            List<int> actual = productCoreObj.Sum<Product>(productList, "cost", 3);
+
+            #endregion
+
+            #region 比較結果
+            Assert.AreEqual(expected[0], actual[0]);
+
+            #endregion
         }
+
+        [TestMethod]
+        public void 按照四組總合Cost值()
+        {
+            #region 假資料
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product()
+            {
+                id = 1,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 2,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 3,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 4,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+
+            productList.Add(new Product()
+            {
+                id = 5,
+                cost = 100,
+                revenue = 120,
+                sellPrice = 220
+            });
+            #endregion
+
+            #region 期望值
+            List<int> expected = new List<int>() {
+                400,
+                100
+            };
+            #endregion
+
+            #region 結果
+            IProductCore productCoreObj = new ProductCore();
+            List<int> actual = productCoreObj.Sum<Product>(productList, "cost", 4);
+
+            #endregion
+
+            #region 比較結果
+            Assert.AreEqual(expected[1], actual[1]);
+
+            #endregion
+        }
+
     }
 }
