ff0d67b8ab7e047ff02a42dbc168b768482441a5
joyceHong <joyceHong0827@gmail.com>
2016-10-11 10:50:36 +0800

    修改後的內容 1: 修改屬性符合Pascal Case: 2: 將interface 移除 3: 更改變數的名稱 productList => products 4: 修改測試的期望語法  CollectionAssert.AreEqual 5: 修改變數陣列集合的宣告方式

--- ProductCore/ProductCore.cs
+++ ProductCore/ProductCore.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
 
 namespace productTest
 {
-    public interface IProductCore
+    public class ProductCore
     {
-        List<int> Sum<T>(List<T> numbers, string columnName, int maxCount);
-    }
-
-    public class ProductCore:IProductCore
-    {
-
         public List<int> Sum<T>(List<T> numbers, string columnName, int maxCount)
         {
             List<int> result = new List<int>();
@@ -23,7 +12,7 @@ namespace productTest
             var counter = 0;
             numbers.ForEach(n =>
             {
-                number =(int) n.GetType().GetProperty(columnName).GetValue(n, null);
+                number = (int)n.GetType().GetProperty(columnName).GetValue(n, null);
 
                 sum += number;
                 ++counter;
@@ -36,20 +25,20 @@ namespace productTest
                 }
             });
 
-            if (numbers.Count % maxCount != 0) {
+            if (numbers.Count % maxCount != 0)
+            {
                 result.Add(sum);
             }
-            
+
             return result;
         }
     }
 
     public class Product
     {
-        public int id { get; set; }
-        public int cost { get; set; }
-        public int revenue { get; set; }
-        public int sellPrice { get; set; }
-     
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
     }
 }
--- productTest/UnitTest1.cs
+++ productTest/UnitTest1.cs
@@ -1,107 +1,85 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NSubstitute;
-using productTest;
 using System.Collections.Generic;
+
 namespace productTest
 {
     [TestClass]
     public class UnitTest1
     {
-
         [TestMethod]
         public void 按照三組總合Cost的值()
         {
             #region 假資料
-            List<Product> productList = GetProductList();
-            #endregion
+
+            List<Product> products = GetProductList();
+
+            #endregion 假資料
 
             #region 期望值
-            List<int> expected = new List<int>() {
-                300,
-                200
-            };
-            #endregion
+
+            List<int> expected = new List<int>() { 5,6,6,4};
+
+            #endregion 期望值
 
             #region 結果
-            IProductCore productCoreObj = new ProductCore();
-            List<int> actual = productCoreObj.Sum<Product>(productList, "cost", 3);
 
-            #endregion
+            ProductCore productCoreObj = new ProductCore();
+            List<int> actual = productCoreObj.Sum<Product>(products, "Cost", 3);
+
+            #endregion 結果
 
             #region 比較結果
-            CollectionAssert.AreEquivalent(expected, actual);
-            #endregion
+
+            CollectionAssert.AreEqual(expected, actual);
+
+            #endregion 比較結果
         }
 
         [TestMethod]
         public void 按照四組總合Cost值()
         {
             #region 假資料
-            List<Product> productList = GetProductList();
-            #endregion
+
+            List<Product> products = GetProductList();
+
+            #endregion 假資料
 
             #region 期望值
-            List<int> expected = new List<int>() {
-                400,
-                100
-            };
-            #endregion
+            
+            List<int> expected = new List<int>() { 7, 8, 6 };
+
+            #endregion 期望值
 
             #region 結果
-            IProductCore productCoreObj = new ProductCore();
-            List<int> actual = productCoreObj.Sum<Product>(productList, "cost", 4);
 
-            #endregion
+            ProductCore productCoreObj = new ProductCore();
+            List<int> actual = productCoreObj.Sum<Product>(products, "Cost", 4);
+
+            #endregion 結果
 
             #region 比較結果
-            CollectionAssert.AreEquivalent(expected, actual);
 
-            #endregion
+            CollectionAssert.AreEqual(expected, actual);
+
+            #endregion 比較結果
         }
 
         private static List<Product> GetProductList()
         {
-            List<Product> productList = new List<Product>();
-            productList.Add(new Product()
+            var products = new List<Product>()
             {
-                id = 1,
-                cost = 100,
-                revenue = 120,
-                sellPrice = 220
-            });
-
-            productList.Add(new Product()
-            {
-                id = 2,
-                cost = 100,
-                revenue = 120,
-                sellPrice = 220
-            });
-
-            productList.Add(new Product()
-            {
-                id = 3,
-                cost = 100,
-                revenue = 120,
-                sellPrice = 220
-            });
-
-            productList.Add(new Product()
-            {
-                id = 4,
-                cost = 100,
-                revenue = 120,
-                sellPrice = 220
-            });
-
-            productList.Add(new Product()
-            {
-                id = 5,
-                cost = 100,
-                revenue = 120,
-                sellPrice = 220
-            });
-            return productList;
+                        new Product() { Id=1 ,Cost=1 ,Revenue=11,SellPrice=21 },
+                        new Product() { Id=2 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=3 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=4 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=5 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() {Id=6 ,Cost=2 ,Revenue=12,SellPrice=22 },          new Product() { Id=7 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=8 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=9 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=10 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                        new Product() { Id=11 ,Cost=2 ,Revenue=12,SellPrice=22 },
+            };
+            return products;
         }
     }
 }
